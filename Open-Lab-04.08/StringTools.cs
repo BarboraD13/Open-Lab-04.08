2423d3e1414cdd10550d35afec13b7f7f17a551d
BarboraD13 <71934521+BarboraD13@users.noreply.github.com>
2020-10-30 11:01:06 +0100

    ++

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,12 +1,20 @@
-﻿using System;
-
+using System;
+using System.Collections.Generic;
 namespace Open_Lab_04._08
 {
     public class StringTools
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            List<string> lists = new List<string>();
+            foreach (string str in strings)
+            {
+                if (str.Length == 4)
+                {
+                    lists.Add(str);
+                }
+            }
+            return lists.ToArray();
         }
     }
 }
